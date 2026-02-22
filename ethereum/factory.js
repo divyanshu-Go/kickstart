f38ea363282cbb8373db5b383d619fafd95d3faa
divyanshu-Go <divyanshu9304628@gmail.com>
2026-02-22 17:55:22 +0530

    deployed new contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@
 import web3 from './web3';
 import Factory from './build/CampaignFactory.json';
 
-const address = '0x0A3bb20Ef91808e0F249E99250cb70bCb65f1a1B';
+const address = '0x09b49478C19153A09287a6E20dd3E1Bb3C65B1e7';
 
 const instance = new web3.eth.Contract(Factory.abi, address);
 
